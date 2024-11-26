b38f913f736d06d941fbe4a10acb6b23fc74ffbf
Musaddek-Ahmed <musaddek59@gmail.com>
2024-11-26 19:21:33 +0600

    latest update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div className="text-xl text-violet-300 font-bold font-circular-web">
-      <Hero />
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+    </main>
   );
 }

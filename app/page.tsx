8c1fe0d517cf3ed7068c8c776daf7279d2303543
Musaddek-Ahmed <musaddek59@gmail.com>
2025-05-01 13:35:28 +0600

    navbar completed and updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import Hero from "./components/hero";
 import About from "./components/about";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar/>
       <Hero/>
       <About/>
     </main>
